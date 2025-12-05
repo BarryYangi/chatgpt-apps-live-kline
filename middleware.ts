7d1bfb90abb99d98ec1cc202fbc4cfe7fb766e57
BarryYangi <2059484047@qq.com>
2025-12-05 10:36:50 +0800

    feat: remove logger

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import "@/lib/logger";
 
 export function middleware(request: NextRequest) {
   if (request.method === "OPTIONS") {
